ffa8f0a902040d7ca500ebec6bc173d6f7f7ca85
Aman-zishan <amanzishan.az@gmail.com>
2020-05-19 18:20:33 +0530

    added counter

--- assets/js/main.js
+++ assets/js/main.js
@@ -279,7 +279,7 @@
         $(this).prop('Counter', 0).animate({
             Counter: $(this).text()
         }, {
-            duration: 4000,
+            duration: 10000,
             easing: 'swing',
             step: function(now) {
                 $(this).text(Math.ceil(now));
